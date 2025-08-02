4652acb2467db2bb8954c03bd366b845a86875af
Damir <damir.ckelet@gmail.com>
2025-08-02 14:31:40 +0300

    feat: Add next.js app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-'use client'
+'use client';
 
 import dynamic from 'next/dynamic';
+import { SplashScreen } from '@capacitor/splash-screen'; // Added
+import { useEffect } from 'react'; // Added
 
 const App = dynamic(() => import('../components/AppShell'), {
   ssr: !!false,
 });
 
 export default function Page() {
+  useEffect(() => {
+    // Added
+    SplashScreen.hide(); // Added
+  }, []); // Added
+
   return <App />;
 }
--- components/pages/Tabs.tsx
+++ components/pages/Tabs.tsx
@@ -9,16 +9,34 @@ import {
 } from '@ionic/react';
 import { cog, flash, list } from 'ionicons/icons';
 
-import Home from './Feed';
+import Home from './Home';
 import Lists from './Lists';
 import ListDetail from './ListDetail';
 import Settings from './Settings';
 
 const Tabs = () => {
+  const tabs = [
+    {
+      title: 'Home',
+      href: '/home',
+      icon: flash,
+    },
+    {
+      title: 'Lists',
+      href: '/lists',
+      icon: list,
+    },
+    {
+      title: 'Settings',
+      href: '/settings',
+      icon: cog,
+    },
+  ];
+
   return (
     <IonTabs>
       <IonRouterOutlet>
-        <Route path="/feed" render={() => <Home />} exact={true} />
+        <Route path="/home" render={() => <Home />} exact={true} />
         <Route path="/lists" render={() => <Lists />} exact={true} />
         <Route
           path="/lists/:listId"
@@ -26,21 +44,15 @@ const Tabs = () => {
           exact={true}
         />
         <Route path="/settings" render={() => <Settings />} exact={true} />
-        <Route path="" render={() => <Redirect to="/feed" />} exact={true} />
+        <Route path="" render={() => <Redirect to="/home" />} exact={true} />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
-        <IonTabButton tab="tab1" href="/feed">
-          <IonIcon icon={flash} />
-          <IonLabel>Feed</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="tab2" href="/lists">
-          <IonIcon icon={list} />
-          <IonLabel>Lists</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="tab3" href="/settings">
-          <IonIcon icon={cog} />
-          <IonLabel>Settings</IonLabel>
-        </IonTabButton>
+        {tabs.map((tab, index) => (
+          <IonTabButton tab={'tab' + index} key={'tab' + index} href={tab.href}>
+            <IonIcon icon={tab.icon} />
+            <IonLabel>{tab.title}</IonLabel>
+          </IonTabButton>
+        ))}
       </IonTabBar>
     </IonTabs>
   );
